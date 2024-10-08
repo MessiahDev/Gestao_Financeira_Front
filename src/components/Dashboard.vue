47fea8017ce25df75ec98a182a2d5e30eecc947f
MessiahDev <alexmessias_18@yahoo.com.br>
2024-10-08 00:03:14 -0300

    Inserindo gráfico de pizza

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -5,7 +5,7 @@
         <v-col cols="7" class="d-flex justfy-center align-center my-16">
           <v-card width="100%" class="mx-12 elevation-0">
             <v-row>
-              <v-col cols="">
+              <v-col cols="6">
                 <v-card class="elevation-0" width="70%">
                   <h1>Controle</h1>
                   Data completa
@@ -36,8 +36,8 @@
             </v-row>
             <v-row>
               <v-col cols="7">
-                <v-card class="mx-0 px-0 elevation-0 d-flex justify-start align-center" max-height="16vh">
-                  <v-chart :data="chartData" :options="chartOptions" max-width="100%" />
+                <v-card class="mx-0 px-0 elevation-0 d-flex justify-start align-center" max-height="18vh">
+                  <v-chart-bar :data="chartData" :options="chartOptions" max-width="100%" />
                 </v-card>
               </v-col>
               <v-col cols="5" class="d-flex justify-center align-center" max-height="16vh">
@@ -61,8 +61,9 @@
               <v-col cols="12">
                 <v-card class="elevation-0" width="100%">
                   <v-list subheader two-line>
-                    <v-subheader class="black--text"><h2>Hoje</h2></v-subheader>
-
+                    <v-card-title>
+                      Hoje
+                    </v-card-title>
                     <v-card class="elevation-0 overflow-auto" max-height="19vh">
                       <v-list-item v-for="folder in folders" :key="folder.title">
                         <v-list-item-avatar>
@@ -89,8 +90,10 @@
                     </v-card>
 
                     <v-divider></v-divider>
-
-                    <v-subheader class="black--text"><h2>Data completa</h2></v-subheader>
+                    
+                    <v-card-title>
+                      Data completa aqui!
+                    </v-card-title>
 
                     <v-card class="elevation-0 overflow-auto" max-height="19vh">
                       <v-list-item v-for="file in files" :key="file.title">
@@ -123,9 +126,10 @@
               <v-col cols="12">
                 <v-card class="elevation-0">
                   <v-list subheader two-line>
-                    <v-subheader class="black--text"><h2>Gastos totais</h2></v-subheader>
-
                     <v-card class="elevation-0 overflow-auto" max-height="40vh">
+                      <v-card-title>
+                        Para onde vai o dinheiro?
+                      </v-card-title>
                       <v-list-item v-for="folder in folders" :key="folder.title">
 
                         <v-list-item-content>
@@ -147,8 +151,19 @@
             </v-row>
             <v-row>
               <v-col cols="12">
-                <v-card class="my16">
-                  dxfgg
+                
+                <v-card-title>
+                  Distribuição de Gastos
+                </v-card-title>
+                <v-card :elevation="0">
+                  <v-card-text style="height: 200px;">
+                    <v-chart-pie
+                      :data="pieChartData"
+                      :options="pieChartOptions"
+                      :height="200"
+                      :width="200"
+                    />
+                  </v-card-text>
                 </v-card>
               </v-col>
             </v-row>
@@ -161,8 +176,8 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import { Chart, registerables } from 'chart.js';
-import { Bar } from 'vue-chartjs';
+import { Chart, registerables, ChartOptions } from 'chart.js';
+import { Bar, Pie } from 'vue-chartjs';
 
 Chart.register(...registerables);
 
@@ -170,7 +185,8 @@ export default Vue.extend({
   name: 'Dashboard',
 
   components: {
-    'v-chart': Bar,
+    'v-chart-bar': Bar,
+    'v-chart-pie': Pie,
   },
 
   data: () => ({
@@ -179,56 +195,77 @@ export default Vue.extend({
       'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'
     ],
     barData: [10, 15, 20, 25, 10, 13, 5, 20, 10, 15, 15, 25],
+    pieChartData: {
+      labels: ['Categoria A', 'Categoria B', 'Categoria C', 'Categoria D'],
+      datasets: [
+        {
+          label: 'Distribuição de Gastos',
+          data: [30, 20, 25, 25],
+          backgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56',
+            '#4BC0C0',
+          ],
+          hoverBackgroundColor: [
+            '#FF6384',
+            '#36A2EB',
+            '#FFCE56',
+            '#4BC0C0',
+          ],
+        },
+      ],
+    },
     files: [
-        {
-          color: 'blue',
-          icon: 'mdi-clipboard-text',
-          subtitle: 'Jan 20, 2014',
-          title: 'Vacation itinerary',
-        },
-        {
-          color: 'amber',
-          icon: 'mdi-gesture-tap-button',
-          subtitle: 'Jan 10, 2014',
-          title: 'Kitchen remodel',
-        },
-        {
-          color: 'amber',
-          icon: 'mdi-gesture-tap-button',
-          subtitle: 'Jan 10, 2014',
-          title: 'Kitchen remodel',
-        },
-      ],
-      folders: [
-        {
-          subtitle: 'Jan 9, 2014',
-          title: 'Photos',
-        },
-        {
-          subtitle: 'Jan 17, 2014',
-          title: 'Recipes',
-        },
-        {
-          subtitle: 'Jan 28, 2014',
-          title: 'Work',
-        },
-        {
-          subtitle: 'Jan 28, 2014',
-          title: 'Docs',
-        },
-        {
-          subtitle: 'Jan 28, 2014',
-          title: 'Docs',
-        },
-        {
-          subtitle: 'Jan 28, 2014',
-          title: 'Docs',
-        },
-      ],
-      interval: {},
-      value: 0,
-      items: ['Alex', 'Núbia', 'Arimar', 'João', 'Artur', 'Victor'],
-      valueSelect: ['Alex'],
+      {
+        color: 'blue',
+        icon: 'mdi-clipboard-text',
+        subtitle: 'Jan 20, 2014',
+        title: 'Vacation itinerary',
+      },
+      {
+        color: 'amber',
+        icon: 'mdi-gesture-tap-button',
+        subtitle: 'Jan 10, 2014',
+        title: 'Kitchen remodel',
+      },
+      {
+        color: 'amber',
+        icon: 'mdi-gesture-tap-button',
+        subtitle: 'Jan 10, 2014',
+        title: 'Kitchen remodel',
+      },
+    ],
+    folders: [
+      {
+        subtitle: 'Jan 9, 2014',
+        title: 'Photos',
+      },
+      {
+        subtitle: 'Jan 17, 2014',
+        title: 'Recipes',
+      },
+      {
+        subtitle: 'Jan 28, 2014',
+        title: 'Work',
+      },
+      {
+        subtitle: 'Jan 28, 2014',
+        title: 'Docs',
+      },
+      {
+        subtitle: 'Jan 28, 2014',
+        title: 'Docs',
+      },
+      {
+        subtitle: 'Jan 28, 2014',
+        title: 'Docs',
+      },
+    ],
+    interval: {},
+    value: 0,
+    items: ['Alex', 'Núbia', 'Arimar', 'João', 'Artur', 'Victor'],
+    valueSelect: ['Alex'],
   }),
 
   mounted () {
@@ -242,7 +279,7 @@ export default Vue.extend({
 
   computed: {
     chartData() {
-      const currentMonth = new Date().getMonth(); // Obtém o mês atual (0-11)
+      const currentMonth = new Date().getMonth();
 
       return {
         labels: this.monthLabels,
@@ -297,11 +334,26 @@ export default Vue.extend({
         backgroundColor: 'white',
       };
     },
+
+    pieChartOptions(): ChartOptions<'pie'> {
+      return {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            position: 'right',
+          },
+          tooltip: {
+            enabled: true,
+          },
+        },
+      };
+    },
   },
 });
 </script>
 
-<style>
+<style scoped>
 .border-avatar {
   border: 2px solid gray;
 }
